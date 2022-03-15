3c34d7f308d5dcc139d116d4e2072d6432bc0310
William Jackson <william@subtlecoolness.com>
2022-03-15 11:20:25 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # install dependencies
-# apk add --no-cache gcc musl-dev
+apk add --no-cache blas-dev lapack-dev
 
 # build
 pip wheel --no-deps --extra-index-url https://alpine-wheels.github.io/index --requirement requirements.txt

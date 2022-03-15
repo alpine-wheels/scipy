2d8e5fbf09bfb774979483eb4eff54019532ec89
William Jackson <william@subtlecoolness.com>
2022-03-15 11:52:42 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # install dependencies
-apk add --no-cache blas-dev gfortran lapack-dev musl-dev
+apk add --no-cache blas-dev gfortran g++ lapack-dev musl-dev
 
 # build
 pip wheel --no-deps --extra-index-url https://alpine-wheels.github.io/index --requirement requirements.txt

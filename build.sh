40a93663e2ac7ba923687914a7fb231d093a3834
William Jackson <565174+williamjacksn@users.noreply.github.com>
2022-08-04 15:02:31 -0500

    Add openblas to build dependecies

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # install dependencies
-apk add --no-cache blas-dev cmake gfortran g++ lapack-dev meson musl-dev pkgconf
+apk add --no-cache blas-dev cmake gfortran g++ lapack-dev meson musl-dev openblas pkgconf
 
 # build
 pip wheel --no-deps --extra-index-url https://alpine-wheels.github.io/index --requirement requirements.txt
